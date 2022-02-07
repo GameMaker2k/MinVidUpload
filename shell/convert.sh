2115eeeacc6f283de5b2cfbb5de948e9989d7ff5
Kazuki Suzuki Przyborowski <kazuki.przyborowski@gmail.com>
2022-02-06 20:31:35 -0600

    Update convert.sh

--- shell/convert.sh
+++ shell/convert.sh
@@ -11,6 +11,12 @@ fi
 if [ "${2}" == "mp4" ]; then
     VideoOutName="$(basename "${1%.*}.mp4")"
 fi
+if [ "${2}" == "ogv" ]; then
+    VideoOutName="$(basename "${1%.*}.ogv")"
+fi
+if [ "${2}" == "webm" ]; then
+    VideoOutName="$(basename "${1%.*}.webm")"
+fi
 
 if [ ! -f "${1}" ]; then
     if [ "${2}" == "flv" ]; then
@@ -20,6 +26,17 @@ if [ ! -f "${1}" ]; then
     fi
     if [ "${2}" == "mp4" ]; then
         ffmpeg -i "${1}" -deinterlace -stats -sameq -vcodec libx264 -acodec aac -strict experimental -f mp4 "${VideoTMPDIR}/$(basename "${1%.*}.mp4")" 1> "${VideoLOGDIR}/$(basename "${1%.*}.log")" 2>&1
+        # ffmpeg -i "${1}" -deinterlace -stats -sameq -vcodec libx265 -acodec aac -strict experimental -f mp4 "${VideoTMPDIR}/$(basename "${1%.*}.mp4")" 1> "${VideoLOGDIR}/$(basename "${1%.*}.log")" 2>&1
+        rm "${1}" "${VideoLOGDIR}/$(basename "${1%.*}.log")"
+        mv "${VideoTMPDIR}/$(basename "${1%.*}.mp4")" "${VideoDIR}/$(basename "${1%.*}.mp4")"
+    fi
+    if [ "${2}" == "ogv" ]; then
+        ffmpeg -i "${1}" -deinterlace -stats -sameq -vcodec libtheora -acodec libvorbis -strict experimental -f ogg "${VideoTMPDIR}/$(basename "${1%.*}.ogv")" 1> "${VideoLOGDIR}/$(basename "${1%.*}.log")" 2>&1
+        rm "${1}" "${VideoLOGDIR}/$(basename "${1%.*}.log")"
+        mv "${VideoTMPDIR}/$(basename "${1%.*}.mp4")" "${VideoDIR}/$(basename "${1%.*}.mp4")"
+    fi
+    if [ "${2}" == "webm" ]; then
+        ffmpeg -i "${1}" -deinterlace -stats -sameq -vcodec libvpx -acodec libvorbis -strict experimental -f matroska "${VideoTMPDIR}/$(basename "${1%.*}.webm")" 1> "${VideoLOGDIR}/$(basename "${1%.*}.log")" 2>&1
         rm "${1}" "${VideoLOGDIR}/$(basename "${1%.*}.log")"
         mv "${VideoTMPDIR}/$(basename "${1%.*}.mp4")" "${VideoDIR}/$(basename "${1%.*}.mp4")"
     fi
